df41daba5244b7c588ff2d91cdb32bbe8e6df3a1
Hammy Havoc <hammy@previousmagazine.com>
2016-07-26 08:36:31 +0100

    Initial commit.

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'faces-law-android',
+		wp_ws_url : 'https://www.facesoflaw.com/wp-appkit-api/faces-law-android',
+		wp_url : 'https://www.facesoflaw.com',
+		theme : 'q-android',
+		version : '10',
+		app_title : 'Faces of Law',
+		app_platform : 'android',
+		gmt_offset : 1,
+		debug_mode : 'off',
+		auth_key : 'huJbDLmsZ121QSPMPQLHr4wyo9zlKC/_VJhz7/(X4ZDBdcXm54Kvg-Jk2K8xSkux',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
